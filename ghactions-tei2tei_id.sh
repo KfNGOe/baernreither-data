02711b2ca1655eae14c4069ef6b31a6bdd4649c8
rh <richard.hoermann@inode.at>
2023-09-08 17:26:28 +0200

    build git actions

--- ghactions-tei2tei_id.sh
+++ ghactions-tei2tei_id.sh
@@ -11,56 +11,55 @@ outputDir="data/tei_xmlId/"
 
 filesChanged=false;
 
-if ! test -f "$inputDir"
+if ! test -d "$inputDir"
 then
-    echo "creating input dir"
-    mkdir -p "$inputDir"
-fi
-
-if [ -z "$changes" ] 
-then
-    echo "\$changes is empty"
+    echo "no input dir"
 else
-    echo "\$changes is NOT empty"      
-    
-    for changed_file in $changes; do
-        echo "Found changed file: ${changed_file}." #changed file is the path to the file including the filename
-        if [[ "$changed_file" == *"$inputDir"* ]]
-        then
-            #check if file is xml
-            if [[ "$changed_file" == *".xml" ]]
+    if [ -z "$changes" ] 
+    then
+        echo "\$changes is empty"
+    else
+        echo "\$changes is NOT empty"      
+        
+        for changed_file in $changes; do
+            echo "Found changed file: ${changed_file}." #changed file is the path to the file including the filename
+            if [[ "$changed_file" == *"$inputDir"* ]]
             then
-                echo "Found changed tei: ${changed_file}"
-                filesChanged=true;
-                name=$(basename "$changed_file" .xml)
-                echo "changed tei filename: ${name}"  #name of the file without extension
-            fi
+                #check if file is xml
+                if [[ "$changed_file" == *".xml" ]]
+                then
+                    echo "Found changed tei: ${changed_file}"
+                    filesChanged=true;
+                    name=$(basename "$changed_file" .xml)
+                    echo "changed tei filename: ${name}"  #name of the file without extension
 
-            if test -f "$outputDir$name.xml"
-            then
-                echo "removing tei file in output dir"
-                rm "$outputDir$name.xml"
-            fi
+                    if test -f "$outputDir$name.xml"
+                    then
+                        echo "removing tei file in output dir"
+                        rm "$outputDir$name.xml"
+                    fi
 
-            if ! test -f "$outputDir"
-            then
-                echo "creating output dir"
-                mkdir -p "$outputDir"
-            fi            
+                    if ! test -d "$outputDir"
+                    then
+                        echo "creating output dir"
+                        mkdir -p "$outputDir"
+                    fi            
 
-            if test -f "$changed_file"
-            then
-                echo "tei was changed/added. Starting transform"
-                echo "Starting tei to tei with xmlid transformation"
-                export name
-                ./build_tei2tei_id.sh
-            fi
+                    if test -f "$changed_file"
+                    then
+                        echo "tei was changed/added. Starting transform"
+                        echo "Starting tei to tei with xmlid transformation"
+                        export name
+                        ./build_tei2tei_id.sh
+                    fi
 
-            if test -f "$inputDir$name.xml"
-            then
-                echo "removing tei file in input dir"
-                rm "$inputDir$name.xml"
+                    if test -f "$inputDir$name.xml"
+                    then
+                        echo "removing tei file in input dir"
+                        rm "$inputDir$name.xml"
+                    fi
+                fi                
             fi
-        fi
-    done          
+        done          
+    fi
 fi
